6c88e2d398a0853821fd76e888a5178ae04a23e4
rayoz12 <rayoz12@yahoo.com.au>
2024-02-18 15:04:29 +1100

    Preload form with database values

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,34 @@ Handlebars.registerHelper("nullCheck", (value) => {
     return value ? value : "None"; 
 });
 
+Handlebars.registerHelper('json', function(this: any) {
+    return JSON.stringify(this);
+});
+
+Handlebars.registerHelper('isSet', function(value) {
+    return value !== null || value !== undefined;
+});
+
+Handlebars.registerHelper('not', function(value) {
+    return !value;
+});
+
+Handlebars.registerHelper('cleanDietRequirements', function(value: string) {
+    const newValue = value.replace("Vegetarian\n", "").replace("Vegan\n", "").trim();
+    console.log(newValue);
+    if (newValue === "") 
+        return null;
+    else
+        return newValue;
+});
+
+Handlebars.registerHelper('contains', function(value: string, substring: string) {
+    if (value === null) {
+        return false;
+    }
+    return value.includes(substring);
+});
+
 interface InviteeResponse {
     token: string,
     nuptialsResponse?: 'yes' | 'no',
